76ae082e27cafdf7c61662b30182b65f72003a30
Jinwoong Kim <rassayzsixt@gmail.com>
2022-01-12 20:24:02 +0900

    1st commit.

---
+++ resources/js/script.js
@@ -0,0 +1,12 @@
+var slide_box = document.querySelector("#img_slide"),
+    slide = document.querySelector("#img_slide img"),
+    currentIndex = 0,
+    slideCount = slide.length,
+    prevBtn = document.querySelector('#prev'),
+    nextBtn = document.querySelector('#next');
+
+console.log(slideCount);
+
+function slide_prev() {
+
+}
